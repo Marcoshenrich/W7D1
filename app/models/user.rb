343a534e18e35985d90b54814bc4c869bf8ff5c0
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-11-14 15:17:52 -0800

    User miogration completed

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+end
