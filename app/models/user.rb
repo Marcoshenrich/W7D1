94f8def6d4c68da663627ef261c5183a0d124ef7
Marcos <jmarcoshenrich@gmail.com>
2022-11-14 15:40:12 -0800

    controllers complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+    validates :password_digest, presence: true
+    validates :session_token, :username, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil:true
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+
+
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_password?(password)
@@ -11,6 +20,30 @@ class User < ApplicationRecord
     def password=(password)
         @password = password
         self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        pass_obj = BCrypt::Password.new(password_digest)
+        pass_obj.is_password?(password)
+    end
+
+    def reset_session_token
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end 
+
+    private
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe.base64
+        while user.exists?(session_token: token)
+            token = SecureRandom::urlsafe.base64
+        end
+        token
     end
 
 end
